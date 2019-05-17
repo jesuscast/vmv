acdc900fe714e39ded04837f7c3762d34d03ad04
Andres Castaneda <jesus.cast.sosa@gmail.com>
2019-05-17 07:36:00 -0700

    move scope variables out

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,20 @@
 const ctrl = {};
+const $scope = {
+    templateId: "a1_poster",
+    variant: "cover",
+    userImageUrl: "https://yt3.ggpht.com/a/AGF-l7_Gek-5o71NvSFO8pye4YJ-A7rToQnB_nbl8Q=s900-mo-c-c0xffffffff-rj-k-no",
+    colorOverlay: "=",
+    scale: 1.0,
+    flipHorizontal: false,
+    rotationDegrees: 0,
+    frozen: false,
+
+    // translateX, translateY are values in the product print image coord system
+    // NOT the canvas coord system.
+    translateX: 0,
+    translateY: 0,
+}
+
+let windowParams = new URLSearchParams(location.search);
+$scope.userImageUrl = windowParams.get("img")
+// imageParams.set("image", windowParams.get("img"));
--- js/edit.js
+++ js/edit.js
@@ -51,21 +51,7 @@ function getScaledEnclosingQuad(canvas, mask) {
 
 function edit() {
     const $element = $("#image-editor-container");
-    const $scope = {
-        templateId: "a1_poster",
-        variant: "cover",
-        userImageUrl: "https://yt3.ggpht.com/a/AGF-l7_Gek-5o71NvSFO8pye4YJ-A7rToQnB_nbl8Q=s900-mo-c-c0xffffffff-rj-k-no",
-        colorOverlay: "=",
-        scale: 1.0,
-        flipHorizontal: false,
-        rotationDegrees: 0,
-        frozen: false,
 
-        // translateX, translateY are values in the product print image coord system
-        // NOT the canvas coord system.
-        translateX: 0,
-        translateY: 0,
-    }
     controller =  ["$window", "$element", "$scope", "$q", "$timeout", "productImage",
             "imagePreloader", "errorBar", "DEBUG"];
         
