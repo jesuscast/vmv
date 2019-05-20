4b82b10b9ca7042c7c4ffe0f3d55fdbce8a03647
Andres Castaneda <jesus.cast.sosa@gmail.com>
2019-05-19 23:01:30 -0700

    Add link to files

--- create_product.php
+++ create_product.php
@@ -3,7 +3,7 @@ include 'create_product_default_view.php';
 
 function image_editor_view_func() {
 ?>
- <div> Random test</div>
+ <div> Random test <?php plugins_url('js/edit.js', __FILE__) ?></div>
 <?php
 }
 
