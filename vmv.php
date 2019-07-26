bbeba75652a8b01eba8acf10f608fbaf00324318
Andres Castaneda <jesus.cast.sosa@gmail.com>
2019-07-25 23:09:45 -0700

    fix

--- vmv.php
+++ vmv.php
@@ -53,8 +53,8 @@ function get_product_orders_for_user( WP_REST_Request $request ) {
 	$query .= ' or user_login = "'.$user_id.'"';
 	$query .= ' or user_email = "'.$user_id.'"';
 
-	// eturn $query;
-	$user = $wpdb->get_row($query);
+	return $query;
+	// $user = $wpdb->get_row($query);
 
 	// if ($wpdb->last_error) {
 	// 	return new WP_Error( 'selection_error', $wpdb->last_error, array( 'status' => 404 ) );
@@ -63,7 +63,7 @@ function get_product_orders_for_user( WP_REST_Request $request ) {
 	// if (empty($user)) {
 	// 	return new WP_Error( 'no_user', 'User not found', array( 'status' => 404 ) );
 	// }
-	echo json_encode($user);
+	// echo json_encode($user);
 }
 
 add_action( 'rest_api_init', function () {
