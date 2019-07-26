edf27f43ca0093c81fa2eb1fb72fbd42f68ad662
Andres Castaneda <jesus.cast.sosa@gmail.com>
2019-07-25 23:11:51 -0700

    fix

--- vmv.php
+++ vmv.php
@@ -54,7 +54,7 @@ function get_product_orders_for_user( WP_REST_Request $request ) {
 	$query .= ' or user_email = "'.$user_id.'"';
 
 	$user = $wpdb->get_row($query);
-	return "ads"
+	return "ads";
 	// if ($wpdb->last_error) {
 	// 	return new WP_Error( 'selection_error', $wpdb->last_error, array( 'status' => 404 ) );
 	// }
