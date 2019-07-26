5289a2cdccbc948481d8ea9889943c4d0581798c
Andres Castaneda <jesus.cast.sosa@gmail.com>
2019-07-25 23:10:08 -0700

    fix

--- vmv.php
+++ vmv.php
@@ -49,7 +49,7 @@ function get_product_orders_for_user( WP_REST_Request $request ) {
 
 	$query = 'select * from wp_users where';
 	$query .= ' id = "'.$user_id.'"';
-	$query .= ' or user_nicename = '.$user_id;
+	$query .= ' or user_nicename = "'.$user_id.'"';
 	$query .= ' or user_login = "'.$user_id.'"';
 	$query .= ' or user_email = "'.$user_id.'"';
 
