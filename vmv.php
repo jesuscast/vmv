ccaeb886f97eee4436469a8bd87d56577daada36
Andres Castaneda <jesus.cast.sosa@gmail.com>
2019-07-25 21:45:04 -0700

    let's try this out, fuck

--- vmv.php
+++ vmv.php
@@ -18,3 +18,48 @@ function create_product_gallery_func( $atts ){
 add_shortcode( 'create_product_gallery', 'create_product_gallery_func' );
 add_shortcode( 'image_editor_view', 'image_editor_view_func' );
 add_shortcode( 'product_list_view', 'product_list_view_func' );
+
+
+// You can access parameters via direct array access on the object:
+// $param = $request['some_param'];
+   
+// // Or via the helper method:
+// $param = $request->get_param( 'some_param' );
+
+// // You can get the combined, merged set of parameters:
+// $parameters = $request->get_params();
+
+// // The individual sets of parameters are also available, if needed:
+// $parameters = $request->get_url_params();
+// $parameters = $request->get_query_params();
+// $parameters = $request->get_body_params();
+// $parameters = $request->get_json_params();
+// $parameters = $request->get_default_params();
+
+// // Uploads aren't merged in, but can be accessed separately:
+// $parameters = $request->get_file_params();
+function get_product_orders_for_user( WP_REST_Request $request ) {
+	$url_params = $request->get_url_params();
+	$user_id = $url_params['user_id'];
+
+	if (empty($user_id)) {
+		return new WP_Error( 'no_user', 'Invalid user', array( 'status' => 400 ) );
+	}
+
+	$user = $wpdb->get_row('select * from wp_users where'
+		.' user_nicename = '.$user_id
+		.' or user_login = '.$user_id
+		.' or user_email = '.$user_id);
+
+	if (empty($user)) {
+		return new WP_Error( 'no_user', 'User not found', array( 'status' => 404 ) );
+	}
+	return $user;
+}
+
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'vmv', '/orders/(?P<user_id>\d+)', array(
+		'methods' => 'GET',
+		'callback' => 'get_product_orders_for_user',
+	));
+});
